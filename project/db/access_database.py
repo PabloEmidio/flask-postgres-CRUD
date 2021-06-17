5b2604f2816e292a3d299f9c08b900dde2a56060
utrape <pablosud025@gmail.com>
2021-06-17 12:50:42 -0300

    Config database configs

--- project/db/access_database.py
+++ project/db/access_database.py
@@ -13,9 +13,13 @@ class AccessDataBase:
     postgres_access = {value[0]: value[1] for value in db_info.items('POSTGRES_CONNECT')}
     if new_value := os.environ.get('DATABASE_URL'):
         postgres_access['host'] = new_value
-    if new_value := os.environ.get('PASSWORD'):
-        print(new_value)
+    if new_value := os.environ.get('DATABASE_PASSWORD'):
         postgres_access['password'] = new_value
+    if new_value := os.environ.get('DATABASE_DATABASE'):
+        postgres_access['database'] = new_value
+    if new_value := os.environ.get('DATABASE_USER'):
+        postgres_access['database'] = new_value
+        
     table_name = db_info.get('MESSAGES_TABLE', 'table_name')
 
     def __init__(self) -> None:
