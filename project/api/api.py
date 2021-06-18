04de754baed9a6de85349832c585a8ea2bf29467
utrape <pablosud025@gmail.com>
2021-06-18 00:43:40 -0300

    Bug fixes | New features | Code improvement

--- project/api/api.py
+++ project/api/api.py
@@ -7,81 +7,91 @@ from project.db.access_database import AccessDataBase
 def init_app(app: Flask):
     
     @app.route('/')
-    def index():
+    def home():
         return redirect('/messages/')
 
     @app.route('/messages/', methods=['GET'])
     def get_messages():
         dbobj = AccessDataBase()
-        messages = dbobj.get_data()
-        return render_template('index.html', messages=messages)
+        kwargs = {
+            'messages': dbobj.get_data(),
+            'title': 'Home | CRUD'
+        }
+        return render_template('home.jinja2', **kwargs)
     
     @app.route('/message/create/', methods=['GET', 'POST'])
     def insert_message():
+        dbobj = AccessDataBase()
         if request.method == 'POST':
             message_json = {}
             message_json['message_title'] = request.form['title'].strip()
             message_json['author_name'] = request.form['author'].strip()
             message_json['message_text'] = request.form['message'].strip()
-            message_json['creation_date'] = str(datetime.now().date())
-            if not message_json['message_title'] or \
-                not message_json['author_name'] or \
-                not message_json['message_text']:
+            
+            if any(len(field)==0 for field in message_json.values()):
                 warning = 'It must not have field blank'
-                return render_template('create.html', warning=warning)
-            warning = ''
+                return render_template('create.jinja2', warning=warning, title='CREATE | CRUD')
+            if dbobj.get_data(['message_title', message_json['message_title']]):
+                warning = 'Already exist message called ' + message_json['message_title']
+                return render_template('create.jinja2', warning=warning, title='CREATE | CRUD')
             if len(message_json['message_title'])>30:
                 warning = 'Title must not have more than 30 character'
-                return render_template('create.html', warning=warning)
+                return render_template('create.jinja2', warning=warning, title='CREATE | CRUD')
             if len(message_json['author_name'])>30:
                 warning = 'Author must not have more than 30 character'
-                return render_template('create.html', warning=warning)
+                return render_template('create.jinja2', warning=warning, title='CREATE | CRUD')
             if len(message_json['message_text'])>200:
                 warning = 'Message must not have more than 200 character'
-                return render_template('create.html', warning=warning)
-            dbobj = AccessDataBase()
+                return render_template('create.jinja2', warning=warning, title='CREATE | CRUD')
+            message_json['creation_date'] = str(datetime.now().date())
+            
             dbobj.write_data(message_json)
             create_now = dbobj.get_data()[-1][0]
             return redirect(f'/message/{create_now}')
         else:
-            return render_template('create.html')
+            return render_template('create.jinja2', title='CREATE | CRUD')
         
     @app.route('/message/update/<int:message_id>/', methods=['GET', 'POST'])
     def update_message(message_id):
         dbobj = AccessDataBase()
-        message = dbobj.get_data(message_id)
+        db_info = dbobj.get_data(['message_id', message_id])[0]
+        message = [item for item in db_info]
+        message[4] = '/'.join(str(message[4]).split('-')[::-1])
+        
         if request.method == 'POST':
             message_json = {}
             message_json['message_title'] = request.form['title'].strip()
             message_json['author_name'] = request.form['author'].strip()
             message_json['message_text'] = request.form['message'].strip()
-            if not message_json['message_title'] and \
-                not message_json['author_name'] and \
-                not message_json['message_text']:
-                return render_template('update.html', messages=message, blank=True)
-            warning = ''
+            
+            if  all(len(field)==0 for field in message_json.values()):
+                warning = 'One of them must be filled in'
+                return render_template('update.jinja2', message=message, warning=warning)
+            if dbobj.get_data(['message_title', message_json['message_title']]):
+                warning = 'Already exist message called ' + message_json['message_title']
+                return render_template('update.jinja2', message=message, warning=warning)
             if message_json['message_title']:
                 if len(message_json['message_title'])>30:
                     warning = 'Title must not have more than 30 character'
-                    return render_template('create.html', warning=warning)
+                    return render_template('update.jinja2', message=message, warning=warning)
                 args = ['message_title', message_json['message_title']]
                 dbobj.update(message_id, args)
             if message_json['author_name']:
                 if len(message_json['author_name'])>30:
                     warning = 'Author must not have more than 30 character'
-                    return render_template('create.html', warning=warning)
+                    return render_template('update.jinja2', message=message, warning=warning)
                 args = ['author_name', message_json['author_name']]
                 dbobj.update(message_id, args)
             if message_json['message_text']:
                 if len(message_json['message_text'])>200:
                     warning = 'Message must not have more than 200 character'
-                    return render_template('create.html', warning=warning)
+                    return render_template('update.jinja2', message=message, warning=warning)
                 args = ['message_text', message_json['message_text']]
                 dbobj.update(message_id, args)
             return redirect(f'/message/{message_id}/')
         else:
-            return render_template('update.html', messages=message) if message else 'Not Found', 404
-            # return render_template('index.html')
+            return render_template('update.jinja2', message=message, title=f'UPDATE | {message[1]}') if message else 'Not Found', 404
+
     
     @app.route('/message/<int:message_id>/', methods=['GET', 'POST'])
     def access_or_delete_message(message_id):
@@ -90,5 +100,11 @@ def init_app(app: Flask):
             dbobj.remove_data(message_id)
             return redirect('/messages/')
         else:
-            message = dbobj.get_data(message_id)
-            return render_template('index.html', messages=message, access_route=True) if message else 'Not Found', 404
+            db_info = dbobj.get_data(['message_id', message_id])[0]
+            message = [item for item in db_info]
+            message[4] = '/'.join(str(message[4]).split('-')[::-1])
+            return render_template('see.jinja2', message=message, title=f'SEE | {message[1]}') if message else 'Not Found', 404
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template('404.jinja2')
