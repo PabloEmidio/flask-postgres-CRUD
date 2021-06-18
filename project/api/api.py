af3582a146ae52bd7e2697f8b0d1b62dec872505
utrape <pablosud025@gmail.com>
2021-06-18 19:16:26 -0300

    Add status code to not found route

--- project/api/api.py
+++ project/api/api.py
@@ -107,4 +107,4 @@ def init_app(app: Flask):
     
     @app.errorhandler(404)
     def not_found(error):
-        return render_template('404.jinja2')
+        return render_template('404.jinja2'), 404
