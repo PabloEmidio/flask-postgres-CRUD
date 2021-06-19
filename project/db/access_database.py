c626673f1f0c4ef7807117004542b82628acbfa5
utrape <pablosud025@gmail.com>
2021-06-18 21:06:17 -0300

    Take db config out from class

--- project/db/access_database.py
+++ project/db/access_database.py
@@ -1,29 +1,10 @@
-import logging
-import logging.config
-import configparser
-import os
-from typing import List, Union
+from typing import Dict, List, Union
 
 import psycopg2
 
-class AccessDataBase:
-    logging.config.fileConfig('project/db/resources/configs/logging.conf',)
-    logger = logging.getLogger('AccessDataBase')
-    logger.debug('logging.conf got')
-    db_info = configparser.ConfigParser()
-    db_info.read('project/db/resources/database.ini')
-    logger.debug('database.ini got')
-    postgres_access = {value[0]: value[1] for value in db_info.items('POSTGRES_CONNECT')}
-    if new_value := os.environ.get('DATABASE_HOST'):
-        postgres_access['host'] = new_value
-    if new_value := os.environ.get('DATABASE_PASSWORD'):
-        postgres_access['password'] = new_value
-    if new_value := os.environ.get('DATABASE_DATABASE'):
-        postgres_access['database'] = new_value
-    if new_value := os.environ.get('DATABASE_USER'):
-        postgres_access['user'] = new_value
-    table_name = db_info.get('MESSAGES_TABLE', 'table_name')
-    logger.debug('DATABASE INFO CONFIGURED')
+from project.db.config_database import ConfigDatabase
+
+class AccessDataBase(ConfigDatabase):
 
     def __init__(self) -> None:
         self.logger.debug('Init Class AccessDataBase')
